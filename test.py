e59d39527fa81b82b7df39bf96e095c8275c4117
Zhang Shisheng（张石生） <92351934+zhangss-0817@users.noreply.github.com>
2023-08-30 14:52:37 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+@author: Jesse Haviland
+"""
+
+import numpy.testing as nt
+import numpy as np
+import roboticstoolbox as rtb
+import unittest
+import spatialmath as sm
+from spatialmath.pose3d import SE3
+import spatialgeometry as gm
+import roboticstoolbox as rtb
